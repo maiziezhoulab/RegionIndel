f06733cd9277d9059fe63b7ef097178148b441e5
volcano1998 <54039319+volcano1998@users.noreply.github.com>
2024-01-22 14:52:08 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,23 +7,23 @@ chmod -R 777 k8-0.2.4
 chmod -R +x SPAdes-3.13.0-Linux
 cd ..
 
-if ! [ -x "$(command -v samtools)" ];
-then
-    echo 'Error: samtools is not installed...'
-    echo 'using Conda to install...'
-    conda install -c bioconda samtools
-else
-    echo 'using existing samtools...'
-fi
+# if ! [ -x "$(command -v samtools)" ];
+# then
+#     echo 'Error: samtools is not installed...'
+#     echo 'using Conda to install...'
+#     conda install -c bioconda samtools
+# else
+#     echo 'using existing samtools...'
+# fi
 
-if ! [ -x "$(command -v minimap2)" ];
-then
-    echo 'Error: minimap2 is not installed...'
-    echo 'using Conda to install...'
-    conda install -c bioconda minimap2
-else
-    echo 'using existing minimap2...'
-fi
+# if ! [ -x "$(command -v minimap2)" ];
+# then
+#     echo 'Error: minimap2 is not installed...'
+#     echo 'using Conda to install...'
+#     conda install -c bioconda minimap2
+# else
+#     echo 'using existing minimap2...'
+# fi
 
 
 
